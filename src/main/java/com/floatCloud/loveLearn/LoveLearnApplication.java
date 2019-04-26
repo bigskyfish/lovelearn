fc5f95ff94704d5c26805eeeeb23cea7409fb07f
fuchb <fuchb@many-it.com>
2019-04-27 03:15:09 +0800

    spring boot 配置设置

---
+++ src/main/java/com/floatCloud/loveLearn/LoveLearnApplication.java
@@ -0,0 +1,13 @@
+package com.floatCloud.loveLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveLearnApplication.class, args);
+	}
+
+}
