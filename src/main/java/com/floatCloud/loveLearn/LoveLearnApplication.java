e1dd67da648959881cc4ffd1f54f3f2d82e4dc91
fuchb <fuchb@many-it.com>
2019-04-28 01:13:51 +0800

    1：generator 实现实体类的创建 2：集成logback log日志配置 3：全局异常处理；请求返回数据封装工具类

--- src/main/java/com/floatCloud/loveLearn/LoveLearnApplication.java
+++ src/main/java/com/floatCloud/loveLearn/LoveLearnApplication.java
@@ -1,9 +1,11 @@
 package com.floatCloud.loveLearn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.floatCloud.loveLearn.dao")
 public class LoveLearnApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/floatCloud/loveLearn/controller/LoginController.java
+++ src/main/java/com/floatCloud/loveLearn/controller/LoginController.java
@@ -1,16 +1,32 @@
 package com.floatCloud.loveLearn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.floatCloud.loveLearn.common.exception.TestException;
+import com.floatCloud.loveLearn.common.pojo.JsonDataDTO;
+import com.floatCloud.loveLearn.dao.domain.SysUser;
+import com.floatCloud.loveLearn.dao.mapper.SysUserMapper;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
     @RequestMapping("/index")
     @ResponseBody
-    public String index(){
-        String returnStr = "Hello World!";
-        return returnStr;
+    public SysUser index(){
+        SysUser sysUser = sysUserMapper.getUserSelective();
+        return sysUser;
     }
+
+    @RequestMapping("/textException.json")
+    public JsonDataDTO textException(){
+        throw new TestException("default error!");
+        /*return JsonDataDTO.fail("hello Exception!");*/
+    }
+
 }
